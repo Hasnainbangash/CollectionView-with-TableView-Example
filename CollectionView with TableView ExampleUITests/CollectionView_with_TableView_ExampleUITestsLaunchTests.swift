4d6d34f53a01830028e54545e682de4b22b48510
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2024-11-22 09:53:38 +0500

    Initial Commit

---
+++ CollectionView with TableView ExampleUITests/CollectionView_with_TableView_ExampleUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  CollectionView_with_TableView_ExampleUITestsLaunchTests.swift
+//  CollectionView with TableView ExampleUITests
+//
+//  Created by Elexoft on 22/11/2024.
+//
+
+import XCTest
+
+final class CollectionView_with_TableView_ExampleUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
