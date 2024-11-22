ec9e01910443bebfa8badbae3a97b7128163579e
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2024-11-22 11:31:02 +0500

    Constants Custom Class Added and in view controller set the array of displaying imagea data along eith the section type

--- CollectionView with TableView Example/ViewController.swift
+++ CollectionView with TableView Example/ViewController.swift
@@ -8,6 +8,32 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var eData = [
+        EntertainmentApp(sectionType: K.SectionNames.bollywoodMovies, imageGallery: [
+            K.imagesNames.BollywoodMoviesNames.image1,
+            K.imagesNames.BollywoodMoviesNames.image2,
+            K.imagesNames.BollywoodMoviesNames.image3,
+            K.imagesNames.BollywoodMoviesNames.image4,
+            K.imagesNames.BollywoodMoviesNames.image5
+        ]),
+        
+        EntertainmentApp(sectionType: K.SectionNames.punjabiMovies, imageGallery: [
+            K.imagesNames.PunjabiMoviesNames.image1,
+            K.imagesNames.PunjabiMoviesNames.image2,
+            K.imagesNames.PunjabiMoviesNames.image3,
+            K.imagesNames.PunjabiMoviesNames.image4,
+            K.imagesNames.PunjabiMoviesNames.image5
+        ]),
+        
+        EntertainmentApp(sectionType: K.SectionNames.webSeries, imageGallery: [
+            K.imagesNames.WebSeriesNames.image1,
+            K.imagesNames.WebSeriesNames.image2,
+            K.imagesNames.WebSeriesNames.image3,
+            K.imagesNames.WebSeriesNames.image4,
+            K.imagesNames.WebSeriesNames.image5,
+        ]),
+    ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
