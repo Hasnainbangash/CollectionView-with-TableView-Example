73a445094c4e3271081e556b7d0fda1d132f364f
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2024-11-22 10:05:06 +0500

    Add the table view, add the prototype, chnage the row height of table view and cell to 200. make the new custom class for cell

---
+++ CollectionView with TableView Example/MyTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MyTableViewCell.swift
+//  CollectionView with TableView Example
+//
+//  Created by Elexoft on 22/11/2024.
+//
+
+import UIKit
+
+class MyTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
