ec122e76f06b5c37416eaa52ae8f938de411b35d
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2024-11-22 11:00:02 +0500

    Collection cell custom class set

---
+++ CollectionView with TableView Example/MyCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  MyCollectionViewCell.swift
+//  CollectionView with TableView Example
+//
+//  Created by Elexoft on 22/11/2024.
+//
+
+import UIKit
+
+class MyCollectionViewCell: UICollectionViewCell {
+    
+}
